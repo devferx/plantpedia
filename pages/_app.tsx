dbdc5ae056103bb6ba68e8a4b8c54ef60aee0fd3
devferx <ferkki@hotmail.com>
2022-03-26 20:58:25 -0400

    Protegiendo rutas API

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { appWithTranslation } from 'next-i18next'
 import { useServerStyles } from '@ui/ssr'
 import { UIProvider } from '@ui/Provider'
 import { QueryProvider } from '@api/QueryProvider'
-import { SessionProvider as AuthProvider } from 'next-auth/react'
+import { SessionProvider } from '@auth/client'
 
 import '../ui/globals.css'
 
@@ -11,13 +11,13 @@ const NextApp = ({ Component, pageProps }: AppProps) => {
   useServerStyles()
 
   return (
-    <AuthProvider session={pageProps.session}>
+    <SessionProvider session={pageProps.session}>
       <QueryProvider>
         <UIProvider>
           <Component {...pageProps} />
         </UIProvider>
       </QueryProvider>
-    </AuthProvider>
+    </SessionProvider>
   )
 }
 
